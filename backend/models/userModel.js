a786b7b263c15a0f8ef07cea35c7e182bb4daa4b
Amit <learnniggo981@gmail.com>
2025-06-27 10:13:52 +0530

    Hashed passwords and checked validation of data

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 //schema
 const userSchema = new mongoose.Schema(
@@ -28,6 +30,33 @@ const userSchema = new mongoose.Schema(
 );
 
 // Helper functions
+userSchema.statics.validateEmail = function (email) {
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email format");
+  }
+};
+
+userSchema.statics.validatePassword = function (password) {
+  // Check if the password is strong enough
+  if (
+    !validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+  ) {
+    throw new Error(
+      "Password must be at least 8 characters long and contain at least one letter and one number and one special character"
+    );
+  }
+};
+
+// hash password
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 9); // here 9 -> is the salt round
+};
+
 userSchema.statics.findUserByEmail = async function (email) {
   const user = await this.findOne({ email }); // findOne is a mongoose builtin method that searches in the collection connnected to the model
   if (!user) {
@@ -39,11 +68,18 @@ userSchema.statics.findUserByEmail = async function (email) {
 //static functions
 userSchema.statics.register = async function (name, email, password) {
   try {
+    // check if email and password are valid
+    this.validateEmail(email);
+    this.validatePassword(password);
+
+    //hash the password
+    const hashedPassword = await this.hashPassword(password);
+
     // Create the new user
     const user = new this({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     // Save the new user
@@ -52,7 +88,7 @@ userSchema.statics.register = async function (name, email, password) {
     return newUser;
   } catch (error) {
     // Throw an error if something goes wrong
-    throw new Error("Error registering user: " + error.message);
+    throw new Error(`Error registering user: ${error.message}`);
   }
 };
 
@@ -76,13 +112,19 @@ userSchema.statics.login = async function (email, password) {
   try {
     const user = await this.findUserByEmail(email);
 
-    const userMatch = password === user.password;
-
-    if (!userMatch) {
+    if (!user) {
       throw new Error("Invalid email or password");
     }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Invalid email or password");
+    }
+
+    // Login successful
+    return user;
   } catch (error) {
-    // Throw an error if there's an issue during the login process
     throw new Error(`Error logging in: ${error.message}`);
   }
 };
